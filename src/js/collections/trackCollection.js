f718a8a580dbdbd9decc9193b03aae39dc26f71f
Matt Gale <matt@littleball.co.uk>
2014-07-10 10:59:52 +0100

    Added localstorage

--- src/js/collections/trackCollection.js
+++ src/js/collections/trackCollection.js
@@ -2,8 +2,11 @@ var Backbone = require('backbone'),
   channelModel = require('../models/channelModel'),
   cfg = require('../config');
 
+Backbone.LocalStorage = require('backbone.localstorage');
+
 module.exports = Backbone.Collection.extend({
   bpm: cfg.track.bpm,
   steps: cfg.track.steps,
-  model: channelModel
+  model: channelModel,
+  localStorage: new Backbone.LocalStorage("flowTrackCollection")
 });
